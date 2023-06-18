dcc5325aa32a70ae4c7f505c41c61eef82c4faa0
Kranc77 <andrzejkrawiec77@gmail.com>
2023-06-18 16:22:53 +0200

    Start working...

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+# potrzebne biblioteki
 from bs4 import BeautifulSoup
 import requests
 
+# strona która mnie interesuje
 page = requests.get('https://www.ebay.com/sch/i.html?_from=R40&_nkw=dodge+viper&_sacat=0&_sop=20')
 
 soup = BeautifulSoup(page.text, 'html.parser')
