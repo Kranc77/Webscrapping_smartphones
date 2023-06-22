2a26c712c1d123e6cbedb71615887fe728f5b4d1
Kranc77 <andrzejkrawiec77@gmail.com>
2023-06-22 18:56:58 +0200

    Pobiera ceny z pierwszej strony

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ import requests
 page = requests.get('https://www.ebay.com/sch/i.html?_from=R40&_nkw=dodge+viper&_sacat=0&_sop=20')
 
 soup = BeautifulSoup(page.text, 'html.parser')
+
+# znalezienie ceny
+prices = soup.find_all('span', class_='s-item__price')
+
+# wypisanie cen bez znaczników HTML
+for price in prices:
+    print(price.text)
+
+print(len(prices))
